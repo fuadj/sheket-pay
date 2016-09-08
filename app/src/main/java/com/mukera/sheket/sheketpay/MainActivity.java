85108404778c804c9771b6cd67b9a0fa2074ae88
Fuad Seffa <fuadjemal@gmail.com>
2016-09-08 19:30:29 +0300

    Initial Commit

---
+++ app/src/main/java/com/mukera/sheket/sheketpay/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mukera.sheket.sheketpay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
